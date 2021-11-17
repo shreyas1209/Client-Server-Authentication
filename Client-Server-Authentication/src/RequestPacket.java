a2a9d02dd38cbfc13191f2684ae7116f6cb2fe1a
shreyas1209 <shreyas.sesham12@gmail.com>
2021-11-17 14:46:11 +0530

    added packetType ActionPacket

--- Client-Server-Authentication/src/RequestPacket.java
+++ Client-Server-Authentication/src/RequestPacket.java
@@ -1,33 +1,26 @@
 
 public class RequestPacket extends ActionPacket{
-    private String source;
-    private String destination;
-    private String protocol;
-    private String message;
-    private boolean actionComplete;
 
     RequestPacket(){
-        this.source = "127.0.0.1";
-        this.destination = "8000";
-        this.protocol = "TCP/IP";
+
+        super();
+
     }
 
+
     RequestPacket(String source,String destination){
-        this.source = source;
-        this.destination = destination;
-        this.protocol = "TCP/IP";
+
+        super(source,destination);
 
     }
 
     RequestPacket(String source,String destination,String protocol){
-        this.source = source;
-        this.destination = destination;
-        this.protocol = protocol;
-        
 
+        super(source,destination,protocol);
+    
     }
 
-    
+
 
     
     
